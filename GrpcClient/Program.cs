4d28e6213384530094d55378a8c991f4c17c3db8
Alexander Bubnov <AlesBubnov@gmail.com>
2023-04-30 01:54:00 +0300

    add proj

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,67 @@
+﻿using Grpc.Net.Client;
+using BillingC;
+using Grpc.Core;
+
+// создаем канал для обмена сообщениями с сервером
+// параметр - адрес сервера gRPC
+//AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+var httpHandler = new HttpClientHandler();
+httpHandler.ServerCertificateCustomValidationCallback =
+    HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
+using var channel = GrpcChannel.ForAddress("http://localhost:5201");
+// создаем клиент
+var client = new Billing.BillingClient(channel);
+
+//Вывод пользователей
+Console.Write("Пользователи:\n");
+var replyU = client.ListUsers(new None());
+while (await replyU.ResponseStream.MoveNext(new CancellationToken()))
+{
+    var current = replyU.ResponseStream.Current;
+    Console.WriteLine($"Имя: {current.Name} Рейтинг:{current.Rating} Баланс: {current.Amount}");
+}
+Console.WriteLine();
+
+//Эмиссия
+Console.Write("Введите количество монет для эмиссии:");
+var amount = Convert.ToInt32(Console.ReadLine());
+var replyE = client.CoinsEmission(new EmissionAmount() {Amount = amount});
+Console.WriteLine($"{replyE.Status} {replyE.Comment}");
+Console.WriteLine();
+
+//Вывод пользователей
+Console.Write("Пользователи:\n");
+var replyUE = client.ListUsers(new None());
+while (await replyUE.ResponseStream.MoveNext(new CancellationToken()))
+{
+    var current = replyUE.ResponseStream.Current;
+    Console.WriteLine($"Имя: {current.Name} Рейтинг:{current.Rating} Баланс: {current.Amount}");
+}
+Console.WriteLine();
+
+//перемещение монет
+Console.Write("Перемещение монет(по умолчанию с Бориса на Олега):");
+var amountCoins = Convert.ToInt32(Console.ReadLine());
+var replyMC = client.MoveCoins(new MoveCoinsTransaction() { Amount = amountCoins, SrcUser = "boris", DstUser = "oleg" });
+Console.WriteLine($"{replyMC.Status} {replyMC.Comment}");
+Console.WriteLine();
+
+
+//Вывод пользователей
+Console.Write("Пользователи:\n");
+var replyUAM = client.ListUsers(new None());
+while (await replyUAM.ResponseStream.MoveNext(new CancellationToken()))
+{
+    var current = replyUAM.ResponseStream.Current;
+    Console.WriteLine($"Имя: {current.Name} Рейтинг:{current.Rating} Баланс: {current.Amount}");
+}
+Console.WriteLine();
+
+//самая длинная история
+Console.Write("Монета с самой длинной историей:\n");
+var replyHC = client.LongestHistoryCoin(new None());
+Console.WriteLine($"ID: {replyHC.Id}\nИстория:{replyHC.History}");
+Console.WriteLine();
+
+
+Console.ReadKey();
---
+++ GrpcServiceTest/Services/BillingService.cs
@@ -0,0 +1,137 @@
+﻿using Grpc.Core;
+using BillingS;
+
+namespace GrpcServiceTest.Services
+{
+    public class BillingService : Billing.BillingBase
+    {
+        private static List<UserProfile> users = new List<UserProfile>()
+            {
+                new UserProfile()
+                {
+                    Name = "boris",
+                    Rating = 5000
+                },
+                new UserProfile()
+                {
+                    Name = "maria",
+                    Rating = 1000
+                },
+                new UserProfile()
+                {
+                    Name = "oleg",
+                    Rating = 800
+                },
+            };
+        private static List<Coin> Coins = new List<Coin>();
+        /// <summary>
+        /// Получение пользователей
+        /// </summary>
+        public override async Task ListUsers(None none, IServerStreamWriter<UserProfile> userProfiles, ServerCallContext context)
+        {
+            foreach (UserProfile profile in users)
+            {
+                await userProfiles.WriteAsync(profile);
+                await Task.Delay(TimeSpan.FromSeconds(1));
+            }            
+        }
+
+        /// <summary>
+        /// Эмиссия валюты
+        /// </summary>
+        public override Task<Response> CoinsEmission(EmissionAmount emission, ServerCallContext context)
+        {
+            if (emission.Amount < users.Count)
+                return Task.FromResult(new Response
+                {
+                    Status = Response.Types.Status.Unspecified,
+                    Comment = "Количества монет недостаточно для распределения"
+                });
+            CreateCoins(emission.Amount);
+            var sumRating = Convert.ToDouble(users.Sum(x => x.Rating));
+            var emissoinCoins = 0;
+            foreach (var profile in users.OrderBy(x => x.Rating))
+            {
+                var currentPercent = Convert.ToDouble(profile.Rating) / sumRating;
+                var curAmount = currentPercent * emission.Amount;
+                profile.Amount = (long)curAmount;
+                for (int i = 0; i < profile.Amount; i++)
+                {
+                    var coin = Coins.Where(x => String.IsNullOrEmpty(x.History)).FirstOrDefault();
+                    coin.History = $"{profile.Name},";
+                    emissoinCoins++;
+                }
+            }
+            users.FirstOrDefault(x => x.Rating == users.Max(x => x.Rating)).Amount += emission.Amount - emissoinCoins;
+            return Task.FromResult(new Response
+            {
+                Status = Response.Types.Status.Ok,
+                Comment = "Монеты распределены"
+            });
+        }
+
+        /// <summary>
+        /// Создание монет
+        /// </summary>
+        private void CreateCoins(long amount)
+        {
+            for (int i = 0; i < amount; i++)
+            {
+                var coin = new Coin()
+                {
+                    Id = Coins.Count + 1,
+                    History = String.Empty
+                };
+                Coins.Add(coin);
+            }
+        }
+
+
+        public override Task<Response> MoveCoins(MoveCoinsTransaction transaction, ServerCallContext context)
+        {
+            var fromUser = users.First(x => x.Name == transaction.SrcUser);
+            var toUser = users.First(x => x.Name == transaction.DstUser);
+            if(fromUser == null || toUser == null)
+            {
+                return Task.FromResult(new Response
+                {
+                    Status = Response.Types.Status.Unspecified,
+                    Comment = "Не найден пользователь"
+                });
+            }
+
+            if(fromUser.Amount < transaction.Amount )
+            {
+                return Task.FromResult(new Response
+                {
+                    Status = Response.Types.Status.Failed,
+                    Comment = "Недостаточно средств для перевода"
+                });
+            }
+
+            for(int i = 0; i < transaction.Amount; i++)
+            {                   
+                var coin = Coins.FirstOrDefault(x => x.History.Trim().Split(',' , StringSplitOptions.RemoveEmptyEntries).LastOrDefault() == fromUser.Name);
+                var history = Coins.FirstOrDefault().History.Trim().Split(',', StringSplitOptions.RemoveEmptyEntries);
+                coin.History += $"{toUser.Name},";
+                toUser.Amount += 1;
+                fromUser.Amount -= 1;
+            }
+
+            return Task.FromResult(new Response
+            {
+                Status = Response.Types.Status.Ok,
+                Comment = "Транзакция выполнена"
+            });
+        }
+
+        public override Task<Coin> LongestHistoryCoin(None none, ServerCallContext context)
+        {
+            var maxStory = Coins.Max(x => (x.History.Split(',', StringSplitOptions.RemoveEmptyEntries)).Length);
+            var coin = Coins.OrderBy(x => x.Id).FirstOrDefault(x => x.History.Split(',', StringSplitOptions.RemoveEmptyEntries).Length == maxStory);
+            return Task.FromResult(coin);
+        }
+
+    }
+
+}
